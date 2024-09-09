9b0dfd0b855238b0562c053c091082f61a758912
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2024-09-09 23:58:39 +0530

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   darkMode: false, // or 'media' or 'class'
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue, js, ts, jsx, tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%'
+      },
+    },
+  },
+  plugins: [],
+}
